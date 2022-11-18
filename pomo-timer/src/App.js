1e6e2ec47037e087eb715814d17822e26f6f905d
ashtonyoung <youngashton28@gmail.com>
2022-11-18 16:19:12 -0700

    cursed timer

--- pomo-timer/src/App.js
+++ pomo-timer/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
+function Display (props) {
+  const [time, setTime] = useState(props.defaultTime)
+
+  function formatTime(seconds) {
+    if (seconds % 60 > 9) {
+      return `${Math.floor(seconds / 60)}:${seconds % 60}`
+    }
+    return `${Math.floor(seconds / 60)}:0${seconds % 60}`
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(time - 1)
+    }, 1000);
+  })
+
+  return (
+    <div>
+      <h1>
+        {formatTime(time)}
+      </h1>
+      <button onClick={() => setTime(time - 1)}>
+        Decrement Time
+      </button>
+    </div> 
+  )
+}
+
+function Interface() {
+  //modes: timer | break | long-break
+  const [mode, setMode] = useState('timer')
+
+  return (
+    <div>
+      <Display defaultTime={60*25}/>
+    </div>
+  )
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      <Interface />
+      {/* Footer */}
     </div>
   );
 }
